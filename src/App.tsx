8d0e993995e5886aae2b231c02875c58e97c460f
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2022-02-17 16:38:12 +1100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import questions from './components/data.js';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [currentStep, setCurrentStep] = useState(0);
+    const [currentWord, setCurrentWord] = useState(
+        questions[currentStep].answer
+    );
+    const [currentGuess, setCurrentGuess] = useState('');
+
+    function delay(ms: number) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }
+
+    useEffect(
+        () => setCurrentWord(questions[currentStep].answer),
+        [currentStep]
+    );
+    useEffect(() => setCurrentGuess(''), [currentStep]);
+
+    const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        let conditionCheck = currentStep < questions.length - 1;
+        if (currentWord === currentGuess) {
+            if (conditionCheck) {
+                setCurrentStep((x) => ++x);
+                setCurrentGuess('');
+                makeGreen();
+            } else {
+                setCurrentStep(0);
+            }
+        }
+        setCurrentGuess(
+            (e.target as HTMLInputElement).value.toLowerCase().trim()
+        );
+    };
+    const handleSpace = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'Space') {
+            setCurrentGuess('');
+        }
+    };
+
+    async function makeGreen() {
+        setCurrentGuess((x) => 'bwahhahahahahah');
+        await delay(3000);
+        setCurrentGuess((x) => ' ');
+    }
+
+    return (
+        <div className="App">
+            <h2 className={currentGuess === '' ? 'highlight_right' : ''}>
+                This is green if the guess was correct. Press Spacebar to test.
+            </h2>
+            <h3>
+                current step: {currentStep + 1} of {questions.length}
+            </h3>
+            <h1>{questions[currentStep].question}</h1>
+            <input
+                type="text"
+                value={currentGuess}
+                onChange={handleInput}
+                onKeyDown={handleSpace}
+            />
+        </div>
+    );
 }
-
-export default App;
---
+++ src/components/data.js
@@ -0,0 +1,9 @@
+const questions = [
+    { question: 'повлечь за собой', answer: 'entail' },
+    { question: 'сходиться', answer: 'converge' },
+    { question: 'склонять чашу весов - ... the scales', answer: 'tip' },
+    { question: 'мусорить', answer: 'litter' },
+    { question: 'бесцельный', answer: 'aimless' },
+];
+
+export default questions;
